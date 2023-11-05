d86ebe7485808f976dee48a71896a50b483e1320
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-05 23:16:51 +0500

    Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -321,6 +321,32 @@ void CPlayerSpawnEvent::FireGameEvent(IGameEvent* event)
     	}
 
     CCSPlayerController* pCSPlayerController = dynamic_cast<CCSPlayerController*>(pPlayerController);
+    		///TEST
+     		
+		if (pCSPlayerController)
+		{
+    			const CCSPlayerPawnBase* playerPawn = pCSPlayerController->m_hPlayerPawn();
+    			if (playerPawn)
+    			{
+        			char buf[256]; // Создайте буфер для сообщения
+        			sprintf(buf, "Success!");
+        			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
+    			}
+    			else
+			{
+        			char buf[256]; // Создайте буфер для сообщения
+        			sprintf(buf, "WRONG PLAYERPAWN!");
+        			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
+    			}
+		}
+		else
+		{
+    			char buf[256]; // Создайте буфер для сообщения
+    			sprintf(buf, "WRONG CSPlayerController!");
+    			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
+		}
+		///TEST
+	
 	if (!pPlayerController || pPlayerController->m_steamID() == 0) // Ignore bots
 	{
 		return;
@@ -333,31 +359,7 @@ void CPlayerSpawnEvent::FireGameEvent(IGameEvent* event)
 		//	{
 		//	return;
 		//	}
-    		///TEST
-     		
-		if (pCSPlayerController)
-		{
-    			const CCSPlayerPawnBase* playerPawn = pCSPlayerController->m_hPlayerPawn();
-    			if (playerPawn)
-    			{
-        			char buf[256]; // Создайте буфер для сообщения
-        			sprintf(buf, "%s\x04 Success!");
-        			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
-    			}
-    			else
-			{
-        			char buf[256]; // Создайте буфер для сообщения
-        			sprintf(buf, "%s\x04 WRONG PLAYERPAWN!");
-        			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
-    			}
-		}
-		else
-		{
-    			char buf[256]; // Создайте буфер для сообщения
-    			sprintf(buf, "WRONG CSPlayerController!");
-    			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
-		}
-		///TEST
+
 	
 
 		
