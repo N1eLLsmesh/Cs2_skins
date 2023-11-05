e5bfcca2b9c01f1063b2b2bfb0cb2796f5c1979e
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-05 20:02:49 +0500

    Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -312,7 +312,7 @@ void CPlayerSpawnEvent::FireGameEvent(IGameEvent* event)
 	int userid_pawn = event->GetInt("userid_pawn"); // Получите userid_pawn из события "playerspawned"
 
 	
-	CCSPlayerPawnBase* playerPawn = GetPlayerPawnByUserID(userid_pawn); // Реализуйте функцию для поиска playerpawn по userid_pawn
+	CSPlayer* playerPawn = static_cast<CSPlayer*>(UTIL_GetPlayerByIndex(userid_pawn));// Реализуйте функцию для поиска playerpawn по userid_pawn
 
 	g_Skin.NextFrame([hPlayerController = CHandle<CBasePlayerController>(pPlayerController), pPlayerController = pPlayerController]()
 	{
