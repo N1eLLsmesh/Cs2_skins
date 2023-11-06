d370cd11f239ab3dfe9c5d3d4a3800df80d3e4e2
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-06 22:26:10 +0500

    Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -625,7 +625,7 @@ void TestSkinchanger(CCSPlayerController* pPlayerController, CCSPlayerPawnBase* 
 	}
 	char buf[255] = { 0 };
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(6000));
+	//std::this_thread::sleep_for(std::chrono::milliseconds(6000));
 	auto weapon_name = g_WeaponsMap.find(weapon_id);
 	bool isKnife = false;
 	int64_t steamid = pPlayerController->m_steamID();
