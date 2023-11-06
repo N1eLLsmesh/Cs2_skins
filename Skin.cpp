f4783c742fccda763333c54991cd654cc8e01571
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-06 21:24:18 +0500

    Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -362,7 +362,7 @@ void CPlayerSpawnEvent::FireGameEvent(IGameEvent* event)
 		//	{
 		//	return;
 		//	}
-	
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 	CCSPlayerController* pCSPlayerController = dynamic_cast<CCSPlayerController*>(pPlayerController);
     		///TEST
      		
@@ -377,8 +377,8 @@ void CPlayerSpawnEvent::FireGameEvent(IGameEvent* event)
 				
         			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
 				//7 707 1 0///TESTFORCHANGE
-				std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-				//TestSkinchanger(pCSPlayerController, playerPawn, 7, 707, 1, 0.0f);
+				//std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+				TestSkinchanger(pCSPlayerController, playerPawn, 7, 707, 1, 0.0f);
 
 				META_CONPRINTF("CCSPlayerController %lld\n", pCSPlayerController);
 				META_CONPRINTF("CCSPlayerPawnBase %lld\n", playerPawn);
@@ -438,7 +438,7 @@ void Event_PlayerSpawned::FireGameEvent(IGameEvent* event)
 	}
 	else
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+		//std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 		CCSPlayerController* pCSPlayerController = dynamic_cast<CCSPlayerController*>(pPlayerController);
     		///TEST
      		
@@ -454,7 +454,7 @@ void Event_PlayerSpawned::FireGameEvent(IGameEvent* event)
         			FnUTIL_ClientPrint(pPlayerController, 3, buf, nullptr, nullptr, nullptr, nullptr);
 				//7 707 1 0///TESTFORCHANGE
 				//std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-				TestSkinchanger(pCSPlayerController, playerPawn, 7, 707, 1, 0.0f);
+				//TestSkinchanger(pCSPlayerController, playerPawn, 7, 707, 1, 0.0f);
 
 				META_CONPRINTF("CCSPlayerController %lld\n", pCSPlayerController);
 				META_CONPRINTF("CCSPlayerPawnBase %lld\n", playerPawn);
