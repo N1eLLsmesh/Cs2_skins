2b382f44e7f06aebf9d1226836e891c1218e12f2
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-11 23:27:41 +0500

    Update Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -909,7 +909,7 @@ void ThreadUpdate(int64_t steamid, CCSPlayerController* pc, CCSPlayerPawnBase* p
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 		META_CONPRINTF("UPDATESKINS SUCCESS %lld\n");
 
-		if(pc->IsInGame()){
+		if(!pc->IsInGame()){
 			META_CONPRINTF("TestSkinchanger: Invalid player or controller\n");
 			players.erase(steamid);
         		break;
