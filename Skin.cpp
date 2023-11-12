2e5528d57163999bbd200768b53b05dea6628ef3
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-12 19:22:24 +0500

    Update Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -969,7 +969,7 @@ void ThreadUpdate(int64_t steamid, CCSPlayerController* pc, CCSPlayerPawnBase* p
         		//break;
 			//}
     		}
-		players[steamid]=NULL;
+		
 		META_CONPRINTF("UPDATESKINS THREAD DELETE %lld\n");
 	}
 	catch(const std::exception& e)
