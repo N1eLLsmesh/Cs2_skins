e945f6410a5db781910eec4b4083723747c4db0f
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-13 22:34:42 +0500

    Update Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -134,6 +134,7 @@ GiveNamedItem_t FnGiveNamedItem;
 UTIL_ClientPrintAll_t FnUTIL_ClientPrintAll;
 ClientPrint_t FnUTIL_ClientPrint;
 SubClassChange_t FnSubClassChange;
+StateChanged_t FnStateChanged;
 
 #else
 void (*FnEntityRemove)(CGameEntitySystem*, void*, void*,uint64_t) = nullptr;
@@ -141,6 +142,7 @@ void (*FnGiveNamedItem)(void* itemService,const char* pchName, void* iSubType,vo
 void (*FnUTIL_ClientPrintAll)(int msg_dest, const char* msg_name, const char* param1, const char* param2, const char* param3, const char* param4) = nullptr;
 void (*FnUTIL_ClientPrint)(CBasePlayerController *player, int msg_dest, const char *msg_name, const char *param1, const char *param2, const char *param3, const char *param4);
 void (*FnSubClassChange)(const CCommandContext &context, const CCommand &args) = nullptr;
+void (*FnStateChanged)(void* networkTransmitComponent, Z_CBaseEntity* ent, int offset, int16_t a4, int16_t a5) = nullptr;
 #endif
 
 std::map<int, std::string> g_WeaponsMap;
