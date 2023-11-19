efe2ed1e2336761c8787bfed7e25148862d7ce58
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-20 00:09:05 +0500

    Update CBsePlayer.h

--- sdk/CBsePlayer.h
+++ sdk/CBsePlayer.h
@@ -5,11 +5,19 @@
 #include "ehandle.h"
 //#include "shared_classnames.h"
 
-#include <c_baseviewmodel.h>
+//#include <c_baseviewmodel.h>
 
 
-class SC_ViewModel : public CBaseViewModel
+//class SC_ViewModel : public CBaseViewModel
+//{
+//public:
+/	//SCHEMA_FIELD(CHandle<CBasePlayer>, CBaseEntity, m_pPredictionPlayer);
+//};
+
+
+class SC_ViewModel
 {
 public:
-	//SCHEMA_FIELD(CHandle<CBasePlayer>, CBaseEntity, m_pPredictionPlayer);
+	SCHEMA_FIELD(CHandle<SC_ViewModel>, CBaseViewModel, m_hViewModel);
+	//SCHEMA_FIELD(int32_t, CCSPlayerController_InGameMoneyServices, m_iStartAccount);
 };
