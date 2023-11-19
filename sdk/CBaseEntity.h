b75e528dcc20de843cf3425f1a26821a457179bd
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-19 16:31:57 +0500

    Update CBaseEntity.h

--- sdk/CBaseEntity.h
+++ sdk/CBaseEntity.h
@@ -10,6 +10,20 @@
 
 class CBasePlayer;
 
+inline CEntityInstance* UTIL_FindEntityByClassname(CEntityInstance* pStart, const char* name)
+{
+	extern CEntitySystem* g_pEntitySystem;
+	CEntityIdentity* pEntity = pStart ? pStart->m_pEntity->m_pNext : g_pEntitySystem->m_EntityList.m_pFirstActiveEntity;
+
+	for (; pEntity; pEntity = pEntity->m_pNext)
+	{
+		if (!strcmp(pEntity->m_designerName.String(), name))
+			return pEntity->m_pInstance;
+	};
+
+	return nullptr;
+}
+
 class SC_CBaseEntity : public CBaseEntity
 {
 public:
