caa272451fe0bda242473c9cfd30c1bd946a9634
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-24 22:13:48 +0500

    Update CBodyComponentSkeletonInstance.h

--- sdk/CBodyComponentSkeletonInstance.h
+++ sdk/CBodyComponentSkeletonInstance.h
@@ -1,5 +1,7 @@
 #pragma once
-#include "CSkeletonInstance.h"
+//CPlayer_ItemServices.h
+    #include "CPlayer_ItemServices.h"
+//#include "CSkeletonInstance.h"
 #include "schemasystem.h"
 
 class CBodyComponentSkeletonInstance {
