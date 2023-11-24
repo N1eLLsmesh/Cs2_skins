1c306e404fb13c29d0ccf7b501144ed140edc98f
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-24 23:25:58 +0500

    Update CCSPlayer_ViewModelServices.h

--- sdk/CCSPlayer_ViewModelServices.h
+++ sdk/CCSPlayer_ViewModelServices.h
@@ -5,7 +5,7 @@ class CCSPlayer_ViewModelServices {
 public:
 	//NETVAR(CHandle<C_CSGOViewModel>, m_hViewModel, "client.dll!CCSPlayer_ViewModelServices->m_hViewModel")
   //SCHEMA_FIELD(CHandle<C_CSGOViewModel>,);
-  //SCHEMA_FIELD(CHandle<C_CSGOViewModel>, CCSPlayer_ViewModelServices, m_hViewModel);
-	SCHEMA_FIELD(C_CSGOViewModel*, CCSPlayer_ViewModelServices, m_hViewModel);
+  SCHEMA_FIELD(CHandle<C_CSGOViewModel>, CCSPlayer_ViewModelServices, m_hViewModel);
+	//SCHEMA_FIELD(C_CSGOViewModel*, CCSPlayer_ViewModelServices, m_hViewModel);
 
 };
