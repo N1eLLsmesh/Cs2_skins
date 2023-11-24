5354898d7dae9956473e86a3809db733a94dc91a
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-24 23:41:28 +0500

    Update CCSPlayerPawn.h

--- sdk/CCSPlayerPawn.h
+++ sdk/CCSPlayerPawn.h
@@ -2,13 +2,10 @@
 #include "CCSPlayerPawnBase.h"
 #include "schemasystem.h"
 //#include "baseviewmodel_shared.h"
-#include "CCSPlayer_ViewModelServices.h"
+//#include "CCSPlayer_ViewModelServices.h"
 
 class CCSPlayerPawn : public CCSPlayerPawnBase
 {
 public:
-      //SCHEMA_FIELD(CbaseViewModel*, CCSPlayerPawn, m_hViewModel);
-
-      //NETVAR(CCSPlayer_ViewModelServices*, m_pViewModelServices, "client.dll!C_CSPlayerPawnBase->m_pViewModelServices");
-	SCHEMA_FIELD(CCSPlayer_ViewModelServices*,CCSPlayerPawn, m_pViewModelServices);
+	//SCHEMA_FIELD(CCSPlayer_ViewModelServices*,CCSPlayerPawn, m_pViewModelServices);
 };
