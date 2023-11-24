5ec4a97d44b2362be52fcbdf06ed21f1dd020fa2
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-24 21:50:51 +0500

    Update C_CSGOViewModel.h

--- sdk/C_CSGOViewModel.h
+++ sdk/C_CSGOViewModel.h
@@ -1,11 +1,12 @@
 #include "schemasystem.h"
 #include "baseentity.h"
+#include "CBaseViewModel.h"
 
-class C_CSGOViewModel: public C_BaseViewModel {
+class C_CSGOViewModel: public CBaseViewModel {
 public:
     SCHEMA_FIELD(CBodyComponent*, C_BaseEntity, m_CBodyComponent);
     SCHEMA_FIELD(CBodyComponentSkeletonInstance*, C_BaseEntity, m_pGameSceneNode);
 
     SCHEMA_FIELD(Vector3, C_BaseFlex, m_CachedViewTarget);
-    SCHEMA_FIELD(CHandle<void*>, C_BaseViewModel, m_iCameraAttachment);
+    SCHEMA_FIELD(CHandle<void*>, CBaseViewModel, m_iCameraAttachment);
 };
