b84218f3c48ff5880e6f4d6f42158085cce9a015
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-24 23:38:52 +0500

    Update CCSPlayerPawnBase.h

--- sdk/CCSPlayerPawnBase.h
+++ sdk/CCSPlayerPawnBase.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "CBasePlayerPawn.h"
 #include "schemasystem.h"
+#include "CCSPlayer_ViewModelServices.h"
 
 class CCSPlayerPawnBase : public CBasePlayerPawn
 {
 public:
 	SCHEMA_FIELD(int32_t, CCSPlayerPawnBase, m_ArmorValue);
 	//SCHEMA_FIELD(CHandle<C_EconItemView*>,CCSPlayerPawnBase,m_EconGloves);
-};
+	SCHEMA_FIELD(CCSPlayer_ViewModelServices*,CCSPlayerPawn, m_pViewModelServices);
+};	
