bea8de814de54c5a0aa70fa9763b3796e33dac07
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-25 00:24:50 +0500

    Update schemasystem.cpp

--- sdk/schemasystem.cpp
+++ sdk/schemasystem.cpp
@@ -18,11 +18,39 @@ CSchemaSystemTypeScope* CSchemaSystem::FindTypeScopeForModule(const char* szpMod
 
 CSchemaSystemTypeScope* CSchemaSystem::GetServerTypeScope()
 {
-    //static CSchemaSystemTypeScope* pServerTypeScope = FindTypeScopeForModule(WIN_LINUX("server.dll", "libserver.so"));
-    static CSchemaSystemTypeScope* pServerTypeScope = FindTypeScopeForModule(WIN_LINUX("client.dll", "libserver.so"));
+    static CSchemaSystemTypeScope* pServerTypeScope = FindTypeScopeForModule(WIN_LINUX("server.dll", "libserver.so"));
+    //static CSchemaSystemTypeScope* pServerTypeScope = FindTypeScopeForModule(WIN_LINUX("client.dll", "libserver.so"));
     
     return pServerTypeScope;
 }
+
+CSchemaSystemTypeScope* CSchemaSystem::GetClientTypeScope()
+{
+    static CSchemaSystemTypeScope* pClientTypeScope = FindTypeScopeForModule(WIN_LINUX("client.dll", "libclient.so"));
+
+    return pClientTypeScope;
+}
+
+int32_t CSchemaSystem::GetClientOffset(const char* pszClassName, const char* pszPropName)
+{
+    SchemaClassInfoData_t* pClassInfo = nullptr;
+    GetClientTypeScope()->FindDeclaredClass(pClassInfo, pszClassName);
+    if (pClassInfo)
+    {
+        for (int i = 0; i < pClassInfo->m_iFieldsCount; i++)
+        {
+            auto& pFieldData = pClassInfo->m_pFieldsData[i];
+
+            if (std::strcmp(pFieldData.m_pszName, pszPropName) == 0)
+            {
+                return pFieldData.m_iOffset;
+            }
+        }
+    }
+
+    return -1;
+}
+
 
 int32_t CSchemaSystem::GetServerOffset(const char* pszClassName, const char* pszPropName)
 {
