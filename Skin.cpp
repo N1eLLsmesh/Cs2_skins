3150ee875c3ab493685cfefd87efd9a43dd98a7d
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-25 23:41:03 +0500

    Update Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -584,12 +584,19 @@ void ForceGlovesUpdate(CCSGOViewModel* viewModel) {
 	
     META_CONPRINTF("viewTargetY %p\n",viewModel->m_viewtarget().y);
     // Передаем значение float вместо указателя и преобразуем long в int64_t
+	try
+		{
    int64_t offset = GetNextSceneEventIDOffset(&viewModel->m_viewtarget().y, &magicNr, magicNr, false);
    META_CONPRINTF("offset %lld\n",offset);
     uint8_t* dataLoc = *reinterpret_cast<uint8_t**>(&viewModel->m_CachedViewTarget().y) + offset * 0x10;
 
 	META_CONPRINTF("dataLoc %lld\n",dataLoc);
     *reinterpret_cast<int*>(dataLoc + 0xc) -= 1;
+		}
+		catch(const std::exception& e)
+		{
+			META_CONPRINTF("offset %s\n",e.what());
+		}
 }
 
 // weird shit to make the gloves update properly
