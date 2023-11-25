b6fe1c41ca17cf3a2d87d80edc80130fd81d239e
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-25 16:59:42 +0500

    Update CCSPlayerPawn.h

--- sdk/CCSPlayerPawn.h
+++ sdk/CCSPlayerPawn.h
@@ -3,9 +3,12 @@
 #include "schemasystem.h"
 //#include "baseviewmodel_shared.h"
 //#include "CCSPlayer_ViewModelServices.h"
+#include "CEconItemView.h"
 
 class CCSPlayerPawn : public CCSPlayerPawnBase
 {
 public:
 	//SCHEMA_FIELD(CCSPlayer_ViewModelServices*,CCSPlayerPawn, m_pViewModelServices);
+
+	SCHEMA_FIELD(C_EconItemView, CCSPlayerPawn, m_EconGloves);
 };
