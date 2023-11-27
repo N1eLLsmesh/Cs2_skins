d2f4c5c27dbb3d3af716b3272679395c424d6d69
N1eLLsmesh <133192477+N1eLLsmesh@users.noreply.github.com>
2023-11-27 23:54:48 +0500

    Update Skin.cpp

--- Skin.cpp
+++ Skin.cpp
@@ -408,7 +408,7 @@ GetNextSceneEventIDOffset = reinterpret_cast<GetNextSceneEventIDOffset_t>(reinte
 	// Signature for sub_F518D0:
 	// 55 48 89 E5 41 57 41 56 41 55 41 54 53 48 81 EC 38 01 00 00 48 89 95 B8 FE FF FF 
 	// \x55\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x41\x54\x53\x48\x81\xEC\x38\x01\x00\x00\x48\x89\x95\xB8\xFE\xFF\xFF
-	constexpr auto PATTERN_FUNCTION_PTR = reinterpret_cast<const uint8_t*>("55 48 89 E5 41 57 41 56 41 55 41 54 53 48 81 EC 38 01 00 00 48 89 95 B8 FE FF FF");
+	constexpr uint8_t PATTERN_FUNCTION_PTR[] = {0x55, 0x48, 0x89, 0xE5, 0x41, 0x57, 0x41, 0x56, 0x41, 0x55, 0x41, 0x54, 0x53, 0x48, 0x81, 0xEC, 0x38, 0x01, 0x00, 0x00, 0x48, 0x89, 0x95, 0xB8, 0xFE, 0xFF, 0xFF};
 	constexpr auto MASK_FUNCTION_PTR = "xxxxxxxxxxxxxxxxxx??xxxxxxxx";
 	constexpr auto OFFSETSTART_FUNCTION_PTR = 1;
 	constexpr auto OFFSETEND_FUNCTION_PTR = 5;
@@ -416,13 +416,8 @@ GetNextSceneEventIDOffset = reinterpret_cast<GetNextSceneEventIDOffset_t>(reinte
 	auto* relCallPtr = libserver.FindPatternSIMD(PATTERN_FUNCTION_PTR, MASK_FUNCTION_PTR).RCast<decltype(GetNextSceneEventIDOffset)>();
 	if (relCallPtr)
 	{
-		int32_t offsetFromInstruction = *reinterpret_cast<int32_t*>(relCallPtr + OFFSETSTART_FUNCTION_PTR);
-	GetNextSceneEventIDOffset = (relCallPtr + OFFSETEND_FUNCTION_PTR + offsetFromInstruction).RCast<decltype(GetNextSceneEventIDOffset)>();
-
-	// Приведение смещения к нужному типу
-	//GetNextSceneEventIDOffset = libserver.FindPatternSIMD("48 85 FF 0F 84 ? ? ? ? 55 31 C0 48 89 E5 41 57").RCast<decltype(GetNextSceneEventIDOffset)>();
-	//GetNextSceneEventIDOffset = reinterpret_cast<GetNextSceneEventIDOffsetFn>(functionAddress + offset);
-	//GetNextSceneEventIDOffset = libserver.FindPatternSIMD("55 48 89 E5 41 57 41 56 41 55 41 54 53 48 81 EC 38 01 00 00 48 89 95 B8 FE FF FF").RCast<decltype(GetNextSceneEventIDOffset)>();
+   		int32_t offsetFromInstruction = *reinterpret_cast<int32_t*>(relCallPtr + OFFSETSTART_FUNCTION_PTR);
+    		GetNextSceneEventIDOffset = (relCallPtr + OFFSETEND_FUNCTION_PTR + offsetFromInstruction).RCast<decltype(GetNextSceneEventIDOffset)>();
 	}
     	
 
